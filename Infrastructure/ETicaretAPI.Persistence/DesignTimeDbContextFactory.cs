f5c84c3d7e82f1e901cf66ad2abb6a55b5b7ba43
ayhan168585 <ayhan168585@hotmail.com>
2025-01-28 01:59:18 +0300

    postgresql bağlantısı yapıldı ve migration oluşturuldu.

---
+++ Infrastructure/ETicaretAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretAPIDbContext>
+    {
+        public ETicaretAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ETicaretAPIDbContext> dbContextOptionBuilder = new();
+            dbContextOptionBuilder.UseNpgsql("User ID=postgres;Password=12345;Host=localhost;Port=5432;Database=ETicaretAPIDb");
+            return new (dbContextOptionBuilder.Options);
+        }
+    }
+}
